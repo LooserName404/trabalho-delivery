8cb5edf7d420463d04d9d7e8019da4398f7d99b7
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2022-04-15 21:33:48 -0300

    chore: adicionar .env e Sequelize

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.SYSTEM_PORT || 3333;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
